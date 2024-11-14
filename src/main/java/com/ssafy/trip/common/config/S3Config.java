4d382aefa4d688adc99871c40a7b8e1bfafa08c3
suhyun <sw92522@naver.com>
2024-11-14 12:30:58 +0900

    feat: S3 bean 생성

--- src/main/java/com/ssafy/trip/common/config/S3Config.java
+++ src/main/java/com/ssafy/trip/common/config/S3Config.java
@@ -1,15 +1,39 @@
 package com.ssafy.trip.common.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "aws-sdk")
 @Getter
 public class S3Config {
-    private String endpoint;
-    private String bucketname;
-    private String accesskey;
-    private String secretkey;
+    @Value("${aws-sdk.endPoint}")
+    private String endPoint;
+
+    @Value("${aws-sdk.regionName}")
+    private String regionName;
+
+    @Value("${aws-sdk.accessKey}")
+    private String accessKey;
+
+    @Value("${aws-sdk.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(regionName)
+                .build();
+    }
 }
