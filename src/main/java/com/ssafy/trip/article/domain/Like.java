9b1ae94213fa8c5db0a55d520576f7cb5fa26fe7
SongJeongHo <dynamite885@naver.com>
2024-11-14 12:22:57 +0900

    feat: 좋아요 Entity 생성

---
+++ src/main/java/com/ssafy/trip/article/domain/Like.java
@@ -0,0 +1,30 @@
+package com.ssafy.trip.article.domain;
+
+import com.ssafy.trip.member.domain.Member;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "likes")
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+}
