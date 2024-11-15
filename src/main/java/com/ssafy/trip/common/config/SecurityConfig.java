655878f40df69e7c64d2a3b1eedc0ae989b57484
SongJeongHo <dynamite885@naver.com>
2024-11-16 02:07:52 +0900

    refactor: 불필요한 설정 제거, csrf설정 메소드 참조 형식으로 변경

--- src/main/java/com/ssafy/trip/common/config/SecurityConfig.java
+++ src/main/java/com/ssafy/trip/common/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
@@ -25,11 +26,11 @@ public class SecurityConfig {
         requestCache.setMatchingRequestParameterName(null);
         // TODO: 프론트 개발 시 CSRF 함께 관리하여 CSRF 보호 활성화하기
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .requestCache(request -> request
                         .requestCache(requestCache))
                 .authorizeHttpRequests((requests) -> requests
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
+                        .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers("/auth/**", "/", "/attractions/**").permitAll()
                         .anyRequest().authenticated())
                 .logout((logout) ->logout
