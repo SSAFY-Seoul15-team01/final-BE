010e2a2b5092bfa719110fdb664d5a220b69b6b7
suhyun <sw92522@naver.com>
2024-11-19 16:35:18 +0900

    feat: filter 요소를 받는 request dto 생성

---
+++ src/main/java/com/ssafy/trip/attraction/dto/AttractionSearchRequest.java
@@ -0,0 +1,41 @@
+package com.ssafy.trip.attraction.dto;
+
+import com.drew.lang.annotations.NotNull;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class AttractionSearchRequest {
+    private static final Integer DEFAULT_CURSOR_ID = 0;
+
+    @NotNull
+    private String keyword;
+
+    private Integer cursorId;
+
+    @NotNull
+    private Boolean spot;
+
+    @NotNull
+    private Boolean facility;
+
+    @NotNull
+    private Boolean festival;
+
+    @NotNull
+    private Boolean leports;
+
+    @NotNull
+    private Boolean stay;
+
+    @NotNull
+    private Boolean shopping;
+
+    @NotNull
+    private Boolean restaurant;
+
+    public Integer getCursorId() {
+        return cursorId != null ? cursorId : DEFAULT_CURSOR_ID;
+    }
+}
